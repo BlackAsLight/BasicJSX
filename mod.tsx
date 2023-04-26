43d40b6f9c9b7b7a449ee88c2fe75fd2e839c1c7
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2023-04-26 22:33:46 +1000

    v3.1.0

--- mod.tsx
+++ mod.tsx
@@ -9,23 +9,22 @@ declare global {
 		interface IntrinsicElements {
 			[ elemName: string ]: Props
 		}
-		interface ElementClass {
-			render: any
-		}
+		// interface ElementClass {
+		// 	render: any
+		// }
 	}
 }
 
 type None = undefined | null
 export type Props = Record<string, string> | None
-// export type Child = string | Tag
 export type Tag = {
 	type: string,
 	props: Props,
 	children: [ string | Tag ] | None
 }
-export type Component = (props: Props, ...tags: [string | Tag]) => Tag
+export type Component = (props: Props, ...tags: [ string | Tag ]) => Tag
 
-export function x(typeOrComponent: string | Component, props: Props, ...children: [string | Tag]): Tag {
+export function x(typeOrComponent: string | Component, props: Props, ...children: [ string | Tag ]): Tag {
 	if (typeof typeOrComponent !== 'string')
 		return typeOrComponent(props, ...children)
 	return {
@@ -40,7 +39,8 @@ export function y(_props: Props, ...children: [ string | Tag ]): [ string | Tag 
 }
 
 export function build<T extends HTMLElement>(tag: Tag): T {
-	const parentElement = createElement(tag)
+	const parentElement = document.createElement(tag.type)
+	appendProps(parentElement, tag.props)
 
 	if (tag.children) {
 		let element = parentElement
@@ -56,15 +56,19 @@ export function build<T extends HTMLElement>(tag: Tag): T {
 			}
 
 			while (pos[ 0 ] < children.length) {
-				// If child is a string, append it.
-				if (typeof children[ pos[ 0 ] ] === 'string')
+				// If child is a not an object, append it.
+				if (typeof children[ pos[ 0 ] ] !== 'object')
 					element.append(children[ pos[ 0 ]++ ] as string)
 				else {
 					// Else move down a scope into the children.
-					element.append(createElement(children[ pos[ 0 ] ] as Tag))
-					element = element.lastChild as HTMLElement
-					children = (children[ pos[ 0 ] ] as Tag).children ?? ([] as any)
-					pos.unshift(0)
+					const e = document.createElement((children[ pos[ 0 ] ] as Tag).type)
+					element.append(e)
+					if (e.toString() !== '[object HTMLUnknownElement]') {
+						appendProps(e, (children[ pos[ 0 ] ] as Tag).props)
+						element = e
+						children = (children[ pos[ 0 ] ] as Tag).children ?? ([] as any)
+						pos.unshift(0)
+					}
 				}
 			}
 			// Move up a scope as current scope has run out of children.
@@ -77,12 +81,10 @@ export function build<T extends HTMLElement>(tag: Tag): T {
 	return parentElement as T
 }
 
-function createElement(tag: Tag): HTMLElement {
-	const element = document.createElement(tag.type)
-	if (tag.props) {
-		const props = Object.entries(tag.props)
-		for (let i = 0; i < props.length; ++i)
-			element.setAttribute(props[ i ][ 0 ], props[ i ][ 1 ])
+function appendProps(element: HTMLElement, props: Props) {
+	if (props) {
+		const array = Object.entries(props)
+		for (let i = 0; i < array.length; ++i)
+			element.setAttribute(array[ i ][ 0 ], array[ i ][ 1 ])
 	}
-	return element
 }
